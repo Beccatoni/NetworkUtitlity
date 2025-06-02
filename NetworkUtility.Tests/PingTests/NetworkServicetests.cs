9d2bb140af123539146cc989cc210ae24a5a7877
Rebecca Mutoni <rbccmtni1gmail.com>
2025-06-02 09:37:15 +0200

    Add project files.

---
+++ NetworkUtility.Tests/PingTests/NetworkServicetests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NetworkUtitlity.Ping;
+using Xunit;
+namespace NetworkUtility.Tests;
+
+public class NetworkServicetests
+{
+   
+    //public  NetworkServicetests()
+    //{
+    //}
+
+    [Fact]
+    public void NetworkSevice_SendPing_ReturnsString()
+    {
+        // Arrange - variables, classes, mocks
+        var pingService = new NetworkService();
+
+        //Act
+        var result = pingService.SendPing();
+
+        // Assert
+        result.Should().NotBeNullOrWhiteSpace();
+        result.Should().Be("Success: Ping Sent!");
+        result.Should().Contain("Success", Exactly.Once());
+    }
+}
---
+++ NetworkUtitlity/Ping/NetworkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtitlity.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            //SearchDNS();
+            //BuildPacket();
+            return "Success: Ping Sent!";
+        }
+    }
+}
